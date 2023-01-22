7fa492dfcccae76f865dee90c3277d01d588dab1
bard hustveit <bard.hustveit@gmail.com>
2023-01-22 10:22:20 +0100

    Added Blazor test client

---
+++ BlazorTestClient/MyLogger.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace BlazorTestClient
+{
+    public class MyLogger : Eloe.InterfaceSerializer.ILogger
+    {
+        public void Debug(string message)
+        {
+        }
+
+        public void Error(string message)
+        {
+        }
+
+        public void Fatal(string message)
+        {
+        }
+
+        public void Info(string message)
+        {
+        }
+
+        public void Warn(string message)
+        {
+        }
+    }
+}
---
+++ BlazorTestClient/WsClient.cs
@@ -0,0 +1,37 @@
+﻿using Eloe.InterfaceRpc;
+using Eloe.InterfaceSerializer;
+using WatsonWebsocket;
+
+namespace BlazorTestClient
+{
+    public class WsClient : InterfaceRpcClientBase
+    {
+        private WatsonWsClient _client;
+
+        public WsClient(Eloe.InterfaceSerializer.ILogger logger)
+            : base(logger, true)
+        {
+        }
+
+        public void Connect(string hostname, int port)
+        {
+            _client = new WatsonWsClient(hostname, port, false);
+            _client.ServerConnected += (s, e) => Console.WriteLine("Connected to server");
+            _client.ServerDisconnected += (s, e) => Console.WriteLine("Disconnected from server");
+
+            _client.MessageReceived += _client_MessageReceived;
+
+            _client.Start();
+        }
+
+        private void _client_MessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+            OnMessageReceived(new MessageReceivedClientArgs { Data = e.Data.ToArray() });
+        }
+
+        protected override Task<bool> SendAsync(SendDataInfo e)
+        {
+            return _client.SendAsync(e.Data);
+        }
+    }
+}
