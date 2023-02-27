b0ba290e8cee8209b5e6ed25363b0dd19c1e8e95
Bård Hustveit <bard.hustveit@apx-systems.com>
2023-02-27 13:11:48 +0100

    refactor get token

--- WebApiClient/Program.cs
+++ WebApiClient/Program.cs
@@ -1,7 +1,6 @@
 ﻿using ClientServerComDef;
 using Eloe.InterfaceRpc;
-using System.Text.Json;
-using WebApiClent;
+using WebApiClient;
 
 internal class Program
 {
@@ -15,54 +14,22 @@ internal class Program
         try
         {
             var tokenUrl = "https://auth.apx-systems.com/connect/token";
-            var httpClient = new HttpClient();
+            var tokenManager = new TokenManager();
+            var token = tokenManager.GetToken(tokenUrl, clientId, clientSecret);
 
-            //Get token using client_credentials flow
-            var content = new FormUrlEncodedContent(new[]
-            {
-                    new KeyValuePair<string,string>("grant_type","client_credentials"),
-                    new KeyValuePair<string, string>("client_id",clientId),
-                    new KeyValuePair<string, string>("client_secret",clientSecret),
-                    new KeyValuePair<string, string>("tenant_id", "AMBE"),
-                    new KeyValuePair<string, string>("scope","api offline_access") // this will get a refresh token in addition to the regular access token, if you dont want that just skip adding "offline_access" to scope
-                });
-
-            httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/x-www-form-urlencoded"));
-
-            Console.WriteLine($"Getting token from: {tokenUrl}");
-            var authRes = httpClient.PostAsync(tokenUrl, content).Result;
-            Console.WriteLine($"Response: {authRes.StatusCode}");
-            string resp = authRes.Content.ReadAsStringAsync().Result;
-
-            //Get token from response
-            var tokenResp = JsonSerializer.Deserialize<TokenResponse>(resp);
-            if (tokenResp == null)
-            {
-                throw new Exception("Unable to get token");
-            }
-            Console.WriteLine($"Token: {tokenResp.access_token.Substring(0, 50)}...");
-
-            var client = new InterfaceRpcSendCollectionHttpClient("https://localhost:7010", tokenResp.access_token);
+            var client = new InterfaceRpcSendCollectionHttpClient("https://localhost:7010", token.access_token);
             var serverInterface = client.AddProxyCallbackInterface<IServerFunctions>();
-
-            var client2 = new InterfaceRpcSendCollectionHttpClient("https://localhost:7010", "");
-            var serverInterface2 = client2.AddProxyCallbackInterface<IServerFunctions>();
-
-            var client3 = new InterfaceRpcSendCollectionHttpClient("https://localhost:7010", "");
-            var serverInterface3 = client3.AddProxyCallbackInterface<IServerFunctions>();
 
             int pingVal = 1;
             while (true)
             {
-                var line = Console.ReadLine();
-                serverInterface.WriteMessage(line);
-                var res = serverInterface2.Ping(pingVal);
-                Console.WriteLine($"ping response: {res}");
-                pingVal++;
-
                 try
                 {
-                    serverInterface3.WriteMessage("line");
+                    var line = Console.ReadLine();
+                    serverInterface.WriteMessage(line);
+                    var res = serverInterface.Ping(pingVal);
+                    Console.WriteLine($"ping response: {res}");
+                    pingVal++;
                 }
                 catch ( Exception ex )
                 {
